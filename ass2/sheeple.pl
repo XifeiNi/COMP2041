1df1cc02e14b75810ab85bf7f1efb92a6f1d4227
XifeiNi <cecilia.ni@student.unsw.edu.au>
2020-08-09 06:03:11 +1000

    Finish while loop evaluation

--- ass2/sheeple.pl
+++ ass2/sheeple.pl
@@ -15,25 +15,39 @@ sub shellToPerl {
 		my $leadingSpaces = getLeadingSpaces($shellLine);
 		$shellLine=~s/^\s*//;
 		chomp $shellLine;
+		if ($shellLine =~ /^#[^!].*/) {
+			push(@perlLines, $shellLine);
+			next;
+		}
+
+		if ($shellLine =~ /\$(\d)/) {
+                        my $arg = int($1) - 1;
+                        $shellLine =~ s?\$(\d)?\$ARGV[$arg]?;
+                }
+
+		if ($shellLine =~ /\$\@/) {
+			$shellLine =~ s?\$\@?\@ARGV?;
+		}
+
 		# start line
 		if ($shellLine =~ /^#!.*/) {
 			push(@perlLines, "#!/usr/bin/perl -w");
 		} 
+
 		# echo
 		elsif ($shellLine =~ /echo (.*)/) {
 			$shellLine =~ s?echo\s*['|"]{0,1}(.*)['|"]{0,1}?$1?;
 			$shellLine =~ s?'??;		
 			$shellLine =~ s?"$??;	
 			$shellLine =~ s?"?\\"?g;
-			if ($shellLine =~ /\$(\d)/) {
-				my $arg = int($1) - 1;
-				$shellLine =~ s?\$(\d)?\$ARGV[$arg]?;
-			}
 			push(@perlLines, $leadingSpaces.qq/print "$shellLine\\n";/);
 		}
+
+		# else
 		elsif ($shellLine =~ /else/) {
 			push(@perlLines, $leadingSpaces."} else {");
-		} 
+		}
+ 
 		# system functions
 		elsif ($shellLine =~ /cd (.*)/ 
 			or $shellLine =~ /pwd/ 
@@ -44,20 +58,32 @@ sub shellToPerl {
 			my $sysLine = qq/system "$shellLine";/;
 			push(@perlLines, $leadingSpaces.$sysLine);
 		}
+
 		# variable assignment
 		elsif ($shellLine =~ /^(\w+)\s*=\s*(.*)$/) {
-			my $assignLine = qq/\$$1 = '$2';/;
+			my $assignLine = "";
+			if (containDollar($2)) {
+				my $newAss = evalLeftHandSide($2);
+				$assignLine = qq/\$$1 = $newAss;/;
+			} else {
+				my $newAss = evalLeftHandSide($2);
+				$assignLine = qq/\$$1 = '$newAss';/;
+			} 
 			push(@perlLines, $leadingSpaces.$assignLine);
 		}
+
+		# then
 		elsif ($shellLine =~ /^then$/) {
 			push(@perlLines, $leadingSpaces."{");
 		}
+
 		# curly bracket at the control block
 		elsif ($shellLine =~ /^done$/
 			or $shellLine =~ /^fi$/) {
 			push(@perlLines, $leadingSpaces."}");
 		}
-		# loop statement
+
+		# loop statement -- for
 		elsif ($shellLine =~ /^for (.*) in (.*)/) {
 			@array =  split(" ", $2);
 			my $loopString = "";
@@ -73,24 +99,34 @@ sub shellToPerl {
 			my $forString = qq/foreach \$$1 $loopString {/;
 			push(@perlLines, $leadingSpaces.$forString);
 		}
+
 		elsif ($shellLine =~ /exit (.*)/) {
 			push(@perlLines, $leadingSpaces.$shellLine);			
 		}
+
 		# read line
 		elsif ($shellLine =~ /read (.*)/) {
 			push(@perlLines, $leadingSpaces."\$$1 = <STDIN>;");
 			push(@perlLines, $leadingSpaces."chomp \$$1;");
 		}
+
 		elsif ($shellLine =~ /^(\w+)\s+test\s+(\w+)\s*(\W+)\s*(\w+)/
 			and !containSlash($shellLine)) {
 			my $if = $1;
 			if ($1 eq "elif") {
 				$if = "} elsif";
 			}
+		
 			my $operator = getOperator($3);
 			push(@perlLines, $leadingSpaces.qq/$if ('$2'$operator'$4')/);
-			
 		}
+
+		# while test $number -le $finish
+		elsif ($shellLine =~ /^while\s+test\s+(\$\w+)\s*(-.+)\s*(\$\w+)/) {
+			my $operator = getOperator($2);
+			push(@perlLines, $leadingSpaces.qq/while ($1$operator$3) {/);
+		}
+
 		# if [ -d /dev/null ]
 		elsif ($shellLine =~ /^(\w+)\s+\[\s+(-\w)\s+(.*)\s+\]/) {
 			my $if = $1;
@@ -116,6 +152,18 @@ sub containSlash {
 	return $string =~ /-/;
 }
 
+sub containDollar {
+	my $string = $_[0];
+	return $string =~ /\$/;
+}
+
+sub evalLeftHandSide {
+	my $string = $_[0];
+	$string =~ s/`//g;
+	$string =~ s/expr//;
+	return $string;	
+}
+
 sub printPerlLines {
 	foreach my $perl (@_) {
 		print "$perl\n";
@@ -126,6 +174,18 @@ sub printPerlLines {
 sub getOperator {
 	my $string = $_[0];
 	my $operator = "";
+	if ($string =~ /le/) {
+		return " <= ";
+	} elsif ($string =~ /lt/) {
+		return " < ";
+	} elsif ($string =~ /eq/) {
+		return " == ";
+	} elsif ($string =~ /ge/) {
+		return " >= ";
+	} elsif ($string =~ /gt/) {
+		return " > ";
+	}
+
 	if ($string =~ /!=/) {
 		$operator = " ne ";
 	} else {
